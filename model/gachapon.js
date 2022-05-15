b449eab1c858debba51e07677f0b0a2570ab4a97
nichamon <nichamon.ve@gmail.com>
2022-05-16 00:54:36 +0700

    edit gachapon  model

--- model/gachapon.js
+++ model/gachapon.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 const gachaponSchema = new mongoose.Schema({
-    admin: {type: mongoose.Schema.Types.ObjectId, ref: 'admin'},
-    title: { type: String },
-    rate_title: { type: String },
-    rate_number: { type: Number },
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'admin' },
+    items: {type: Array},
 });
 
 module.exports = mongoose.model("gachapon", gachaponSchema);
