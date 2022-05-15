96f82d35d231ed356d38ca2a27c5a76014b0b44a
nichamon <nichamon.ve@gmail.com>
2022-05-15 17:43:17 +0700

    Router for  webhook

---
+++ route/webhook.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const User = require("../model/user");
+const authBank = require("../middleware/auth_bank");
+const router = express.Router();
+
+// Payment
+router.post("/bank", authBank, async (req, res) => {
+  try {
+    const { account_id, amount } = req.body;
+    await User.findByIdAndUpdate(account_id, { $inc: { coin: amount } });
+    res.status(200).send("Success");
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+module.exports = router;
