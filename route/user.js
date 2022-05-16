ce9b9b7852ed3e2d7d831e2c38b58f13a7982f62
nichamon <nichamon.ve@gmail.com>
2022-05-16 11:30:15 +0700

    change variable type

--- route/user.js
+++ route/user.js
@@ -11,7 +11,7 @@ const router = express.Router();
 const config = process.env;
 
 function playGachapon(rank, gachas) {
-  var rand = Math.floor(Math.random() * 101);
+  const rand = Math.floor(Math.random() * 101);
   let item = "";
   switch (rank) {
     case "bronze":
